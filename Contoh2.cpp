a0844bd59a966db5a23a18b9cea7585a744f71d5
rnktlhr <rinakitluhur10@gmail.com>
2024-04-01 14:53:53 +0700

    menambahkan cout dan cin nama kota pada int main

--- Contoh2.cpp
+++ Contoh2.cpp
@@ -22,6 +22,8 @@ int main(){
     cout << "Alamat Mahasiswa: " << endl;
     cout << "\t Nama Desa: ";
     cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota: ";
+    cin >> mhs.alamat.kota;
     cout << "Umur Mahsiswa: ";
     cin >> mhs.umur;
 
