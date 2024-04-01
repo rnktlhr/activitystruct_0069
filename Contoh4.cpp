13ad3645314b07f984d4c1b686b0b19a87f02dee
rnktlhr <rinakitluhur10@gmail.com>
2024-04-01 15:14:08 +0700

    Menambahkan for dan  array pada int main

--- Contoh4.cpp
+++ Contoh4.cpp
@@ -14,24 +14,29 @@ struct Mahasiswa{
 };
 
 int main(){
-    Mahasiswa mhs;
-    cout << "Nomer Mahasiswa: ";
-    getline(cin, mhs.nim);
-    cout << "Nama Mahasiswa: ";
-    cin >> mhs.nama;
-    getline(cin, mhs.nama);
-    cout << "Alamat Mahasiswa: " << endl;
-    cout << "\t Nama Desa: ";
-    cin >> mhs.Alamat.desa;
-    cout << "\t Nama Kota: ";
-    cin >> mhs.Alamat.kota;
-    cout << "Umur Mahsiswa: ";
-    cin >> mhs.umur;
+    Mahasiswa mhs[3];
+    for(int i=0;i<3;i++){
+        cout << "Data ke- " << (i+1)<<":"<<endl;
+        cout << "Nomer Mahasiswa: ";
+        getline(cin, mhs[i].nim);
+        cout << "Nama Mahasiswa: ";
+        cin >> mhs[i].nama;
+        getline(cin, mhs[i].nama);
+        cout << "Alamat Mahasiswa: " << endl;
+        cout << "\t Nama Desa: ";
+        cin >> mhs[i].Alamat.desa;
+        cout << "\t Nama Kota: ";
+        cin >> mhs[i].Alamat.kota;
+        cout << "Umur Mahsiswa: ";
+        cin >> mhs[i].umur;
+    }
 
-    cout << "\n NIM: " << mhs.nim;
-    cout << "\n Nama: " << mhs.nama;
+    for(int i=0;i<3;i++){
+    cout << "\n NIM: " << mhs[i].nim;
+    cout << "\n Nama: " << mhs[i].nama;
     cout << "\n Alamat: ";
-    cout << "\n \t Desa: " << mhs.Alamat.desa;
-    cout << "\n \t Kota: " << mhs.Alamat.kota;
-    cout << "\n Umur: " << mhs.umur;
+    cout << "\n \t Desa: " << mhs[i].Alamat.desa;
+    cout << "\n \t Kota: " << mhs[i].Alamat.kota;
+    cout << "\n Umur: " << mhs[i].umur;
+    }
 }
